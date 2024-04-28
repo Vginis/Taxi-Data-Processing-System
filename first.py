2576aae57c3a4f61e3f986f8994cd047da2f3409
AMoraitou <148851282+AMoraitou@users.noreply.github.com>
2024-04-28 14:34:21 +0300

    Update first.py to save single csv

--- first.py
+++ first.py
@@ -1,38 +1,66 @@
-import pandas as panda
+def get_quadrant(latitude, longitude, center_lat=40.735923, center_lon=-73.990294):
+    if latitude >= center_lat:
+        if longitude >= center_lon:
+            return "Q1"
+        else:
+            return "Q2"
+    else:
+        if longitude >= center_lon:
+            return "Q3"
+        else:
+            return "Q4"
+
+
+# Set the data location and type
 storage_account_name = "taxibatchdata"
 storage_account_access_key = "3Gbq5k8yW9PsBALgsHwC56xUZfJDvuFdkjxz4av5pMPSwJNcegLlXyoE/O4S/+TrSMWrQNqLH5zC+AStryI3xg=="
-file_location = "wasbs://storagecontainer@taxibatchdata.blob.core.windows.net/2009-01-31.csv"
+file_location = (
+    "wasbs://storagecontainer@taxibatchdata.blob.core.windows.net/2009-01-31.csv"
+)
 file_type = "csv"
+
 spark.conf.set(
-  "fs.azure.account.key."+storage_account_name+".blob.core.windows.net",
-  storage_account_access_key)
-df = spark.read.csv(file_location, header=True, inferSchema=True)
-print(df)
-rows = df.select("pickup_latitude","pickup_longitude").collect()
-q1 =0
-q2 = 0
-q3 =0 
-q4 = 0
-for i in rows:
-  if(i.pickup_latitude>=40.735923 and i.pickup_longitude>=-73.990294):
-    q1+=1
-  elif (i.pickup_latitude<40.735923 and i.pickup_longitude>=-73.990294):
-    q2+=1
-  elif (i.pickup_latitude<40.735923 and i.pickup_longitude<-73.990294):
-    q3+=1
-  else:
-    q4+=1
-print(q1)
-print(q2)
-print(q3)
-print(q4)
+    "fs.azure.account.key." + storage_account_name + ".blob.core.windows.net",
+    storage_account_access_key,
+)
 
-output = [
-  ("Q1", q1),
-  ("Q2", q2),
-  ("Q3", q3),
-  ("Q4", q4)
-]
+# Read the CSV file
+dataDF = (
+    spark.read.format(file_type)
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .load(file_location)
+)
 
-sparkDF2 = spark.createDataFrame(output,["Qs", "output"])
-sparkDF2.coalesce(1).write.csv("wasbs://storagecontainer@taxibatchdata.blob.core.windows.net/firstOutput.csv",header=True)
+# Count rides by quadrant
+quadrants = {"Q1": 0, "Q2": 0, "Q3": 0, "Q4": 0}
+rows = dataDF.select("pickup_latitude", "pickup_longitude").collect()
+for r in rows:
+    pickup_lat = r.pickup_latitude
+    pickup_lon = r.pickup_longitude
+
+    quadrant = get_quadrant(pickup_lat, pickup_lon)
+    quadrants[quadrant] += 1
+
+# Print the results
+for quadrant, count in quadrants.items():
+    print(f"{quadrant}: {count} rides")
+
+# Convert dictionary to Spark DataFrame
+quadrants_df = spark.createDataFrame(
+    [(k, v) for k, v in quadrants.items()], ["Quadrant", "Rides"]
+)
+
+# Write DataFrame to CSV file
+quadrants_df.coalesce(1).write.csv(
+    "wasbs://storagecontainer@taxibatchdata.blob.core.windows.net/address-temp",
+    header=True,
+)
+
+file_path = [file.path for file in dbutils.fs.ls("wasbs://storagecontainer@taxibatchdata.blob.core.windows.net/address-temp/")
+             if file.name.endswith(".csv")][0]
+
+dbutils.fs.cp(file_path,"wasbs://storagecontainer@taxibatchdata.blob.core.windows.net/firstOutput.csv")
+dbutils.fs.rm("wasbs://storagecontainer@taxibatchdata.blob.core.windows.net/address-temp",recurse=True)
+
+print("Quadrant counts saved to firstOutput.csv")
