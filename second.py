b66f369f1ad2796ce9dd6cdb66320146b8a1f18d
vginis <vginis10@gmail.com>
2024-05-04 20:07:48 +0300

    secondUpdate

--- second.py
+++ second.py
@@ -1,21 +1,12 @@
+storage_container_name = "storagecontainer"
+storage_account_name = "taxibatchdata"
+storage_account_access_key = "BVy7eawZezL2IJfqfaapyTM1b71EvEZ0ksc6uPGJqXWMxC+hj0IxuIDbRO9kq1Afp++0I9DRZZds+AStfK1+gA=="
+
+dbutils.widgets.text("fileName","","")
+fileName = "wasbs://"+storage_container_name+"@"+storage_account_name+".blob.core.windows.net/Data/"+ dbutils.widgets.get("fileName")
 import csv
 from math import radians, cos, sin, asin, sqrt
 
-def main():
-    with open("CSV per Month/2009-01-31.csv","r") as file:
-        reader = csv.reader(file)
-        next(reader, None)
-        output = []
-        for row in reader:
-            if float(row[1])>=10 and int(row[7])>=2 and haversine(float(row[3]),float(row[4]),float(row[5]),float(row[6]))>=1:
-                output.append(row)
-                print(haversine(float(row[3]),float(row[4]),float(row[5]),float(row[6])))
-        with open('output1.csv','w',newline='') as file2:
-            writer = csv.writer(file2)
-            for i in output:
-                writer.writerow(i)
-            file2.close()
-        file.close()            
 def haversine(lon1, lat1, lon2, lat2):
     # convert decimal degrees to radians 
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
@@ -26,4 +17,43 @@ def haversine(lon1, lat1, lon2, lat2):
     c = 2 * asin(sqrt(a)) 
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
     return c * r
-main()
+
+spark.conf.set(
+    "fs.azure.account.key." + storage_account_name + ".blob.core.windows.net",
+    storage_account_access_key,
+)
+# Read the Data CSV file
+countsDF=spark.read.option("header","true").option("inferSchema","true").csv("wasbs://"+storage_container_name+"@"+storage_account_name+".blob.core.windows.net/output2.csv")
+dataDF=spark.read.option("header","true").option("inferSchema","true").csv(fileName)
+# Count rides by quadrant
+routes = []
+dataRows = dataDF.collect()
+for row in dataRows:
+    if float(row[1])>=10 and int(row[7])>=2 and haversine(float(row[3]),float(row[4]),float(row[5]),float(row[6]))>=1:
+        routes.append(row)
+        print(haversine(float(row[3]),float(row[4]),float(row[5]),float(row[6])))
+for d in routes:
+    print(d)
+countsDF = spark.createDataFrame(routes)
+#Write DataFrame to CSV file
+countsDF.coalesce(1).write.csv(
+    "wasbs://"+storage_container_name+"@"+storage_account_name+".blob.core.windows.net/address-temp",
+    header=True,
+)
+print("Here")
+file_path = [
+    file.path
+    for file in dbutils.fs.ls(
+        "wasbs://"+storage_container_name+"@"+storage_account_name+".blob.core.windows.net/address-temp"
+    )
+    if file.name.endswith(".csv")
+][0]
+
+dbutils.fs.cp(
+    file_path,
+    "wasbs://"+storage_container_name+"@"+storage_account_name+".blob.core.windows.net/output2.csv",
+)
+dbutils.fs.rm(
+    "wasbs://"+storage_container_name+"@"+storage_account_name+".blob.core.windows.net/address-temp",
+    recurse=True,
+)
